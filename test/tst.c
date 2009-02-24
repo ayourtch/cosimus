90f84eafb9c42d972a4ec9d24c84d7f820f1a66f
Andrew Yourtchenko <ayourtch+trillian@gmail.com>
2009-02-24 03:57:44 +0100

    print the size of uuid on startup.

--- test/tst.c
+++ test/tst.c
@@ -1,11 +1,12 @@
 #include "lib_debug.h"
 #include "lib_httpd.h"
 #include "lib_sock.h"
+#include "lib_uuid.h"
 
 int main(int argc, char *argv[])
 {
   set_debug_level(DBG_GLOBAL, 100);
-  debug(0,0, "Hello there!");
+  debug(0,0, "Hello there, sizeof of uuid_t: %d!", sizeof(uuid_t));
   http_start_listener("127.0.0.1", 12345, NULL);
   http_start_listener("127.0.0.1", 12346, NULL);
   while (1) {
