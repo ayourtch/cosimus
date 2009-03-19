5327be727732c132adcab2959673d8626f4364b1
Andrew Yourtchenko <ayourtch+trillian@gmail.com>
2009-03-19 22:19:17 +0100

    expose a few more FMV functions to Lua

--- fmv_src/lua_fmv.c
+++ fmv_src/lua_fmv.c
@@ -1,3 +1,4 @@
+#include "sta_fmv.h"
 #include "lua_fmv.h"
 #include "lib_dbuf.h"
 
@@ -33,12 +34,28 @@ lua_fn_packet_unlock(lua_State *L) {
   return 0;
 }
 
+static int
+lua_fn_GetSequenceNumber(lua_State *L) {
+  dbuf_t *d = luaL_checkuserdata(L, 1);
+  lua_pushinteger(L, GetSequenceNumber(d->buf));
+  return 1;
+}
+
+static int
+lua_fn_SetSequenceNumber(lua_State *L) {
+  dbuf_t *d = luaL_checkuserdata(L, 1);
+  u32t seq = luaL_checkint(L, 2);
+  SetSequenceNumber(d->buf, seq);
+  return 0;
+}
 
 
 const luaL_reg fmv_sta_lib[] = {
   { "packet_new", lua_fn_packet_new },
   { "packet_lock", lua_fn_packet_lock },
   { "packet_unlock", lua_fn_packet_unlock },
+  { "GetSequenceNumber", lua_fn_GetSequenceNumber },
+  { "SetSequenceNumber", lua_fn_SetSequenceNumber },
   { NULL, NULL }
 };
 
