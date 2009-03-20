5470eb6a58c19952a328ecaba547e4722a4b1687
Andrew Yourtchenko <ayourtch+trillian@gmail.com>
2009-03-20 02:39:32 +0100

    starting to pull out the dynamic state for the lib

--- supp_src/lib_sock_intern.h
+++ supp_src/lib_sock_intern.h
@@ -62,8 +62,18 @@ typedef struct {
   sock_handlers_t handlers;     /* structure with the function pointers for the handlers */
 } conndata_t;
 
+typedef struct {
+  struct pollfd ufds[MAX_FDS];
+  conndata_t cdata[MAX_FDS];
+  int nfds;
+  int nclients;               /* number of active inbound connections */
+  long nconnections;          /* counter for incoming connections */
+  int biggest_udp_idx;        /* the biggest index of the UDP sockets */
+} lib_sock_runtime_data_t;
 
 
+
+#ifdef XXXXX
 extern struct pollfd ufds[MAX_FDS];
 extern conndata_t cdata[MAX_FDS];
 extern int nfds;
@@ -73,7 +83,7 @@ int nclients;
 long nconnections; 
 
 int biggest_udp_idx; 
-
+#endif
 int do_l7_reset(int idx);
 
 
