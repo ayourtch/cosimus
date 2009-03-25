ebcb4358a2a240443a2d2bffb0c281e0f3b596f2
Andrew Yourtchenko <ayourtch+trillian@gmail.com>
2009-03-25 02:33:45 +0100

    more refs to libsupp_init

--- test/tst.c
+++ test/tst.c
@@ -6,7 +6,6 @@
 int main(int argc, char *argv[])
 {
   set_debug_level(DBG_GLOBAL, 100);
-  libsupp_init(NULL);
   debug(0,0, "Hello there, sizeof of uuid_t: %d!", sizeof(uuid_t));
   http_start_listener("127.0.0.1", 12345, NULL);
   http_start_listener("127.0.0.1", 12346, NULL);
