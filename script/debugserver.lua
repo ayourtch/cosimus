08c46c1a1da7c3615351d34881e8ace5619f016a
Andrew Yourtchenko <ayourtch@gmail.com>
2010-05-02 20:44:28 +0200

    add the web evaluator for debugserver

--- script/debugserver.lua
+++ script/debugserver.lua
@@ -28,6 +28,54 @@ require "httpd_common"
 
 debugserver = {}
 
+
+-- a few functions from cgilua for parsing the query
+
+function unescape (str)
+        str = string.gsub (str, "+", " ")
+        str = string.gsub (str, "%%(%x%x)", function(h) return string.char(tonumber(h,16)) end)
+        str = string.gsub (str, "\r\n", "\n")
+        return str
+end
+
+function escape (str)
+        str = string.gsub (str, "\n", "\r\n")
+        str = string.gsub (str, "([^0-9a-zA-Z ])", -- locale independent
+                function (c) return string.format ("%%%02X", string.byte(c)) end)
+        str = string.gsub (str, " ", "+")
+        return str
+end
+
+
+function insertfield (args, name, value)
+        if not args[name] then
+                args[name] = value
+        else
+                local t = type (args[name])
+                if t == "string" then
+                        args[name] = {
+                                args[name],
+                                value,
+                        }
+                elseif t == "table" then
+                        table.insert (args[name], value)
+                else
+                        error ("CGILua fatal error (invalid args table)!")
+                end
+        end
+end
+
+function parsequery (query, args)
+        if type(query) == "string" then
+                local insertfield, unescape = insertfield, unescape
+                string.gsub (query, "([^&=]+)=([^&=]*)&?",
+                        function (key, val)
+                                insertfield (args, unescape(key), unescape(val))
+                        end)
+        end
+end
+
+
 function debug_server_http(uri, appdata, dh, dd)
   local qstring = su.get_http_data(appdata, "querystring")
   local pdata = su.get_http_data(appdata, "post_data")
@@ -41,33 +89,49 @@ function debug_server_http(uri, appdata, dh, dd)
 
   local path = split("/" .. uri, "[/]")
   local tmp = ""
+  local args = {}
   su.dstrcat(dd, "<html><body>")
 
-  for i, el in ipairs(path) do
-    tmp = tmp .. el 
-    if tmp ~= "/" then
-      tmp = tmp .. "/"
-    end
-    su.dstrcat(dd, '<a href="' .. tmp .. '">' .. el .. '/</a> ')
-    if tbl[el] then
-      tbl = tbl[el] 
-    else
-      print("Could not fetch key '" .. el .. "'\n")
-    end
-  end
-  pp('<hr size=1 noshade>')
-  pp("<ul>\n")
+  if pdata then
+    parsequery(pdata, args)
+    assert(loadstring(args.eval, "web-input"))()
+  else
 
-  for k, v in pairs(tbl) do
-    pp("<li>")
-    if (type(v) == "table") then
-      pp("<a href='" .. tostring(k) .. "/'>" .. tostring(k) .. "</a>")
-    else
-      pp(tostring(k) .. " : " .. tostring(v))
+    for i, el in ipairs(path) do
+      tmp = tmp .. el 
+      if tmp ~= "/" then
+	tmp = tmp .. "/"
+      end
+      su.dstrcat(dd, '<a href="' .. tmp .. '">' .. el .. '/</a> ')
+      if tbl[el] then
+	tbl = tbl[el] 
+      else
+	print("Could not fetch key '" .. el .. "'\n")
+      end
     end
-    pp("</li>\n")
-  end 
-  pp("</ul>")
+    pp('<hr size=1 noshade>')
+    pp("<ul>\n")
+
+    for k, v in pairs(tbl) do
+      pp("<li>")
+      if (type(v) == "table") then
+	pp("<a href='" .. tostring(k) .. "/'>" .. tostring(k) .. "</a>")
+      else
+	pp(tostring(k) .. " : " .. tostring(v))
+      end
+      pp("</li>\n")
+    end 
+    pp("</ul>")
+  end
+  pp("<hr size=1 noshade>")
+  pp("<form method='post'>")
+  pp("<input type='submit'><br>")
+  pp("<textarea name='eval' rows=25 cols=80>")
+  if args.eval then
+    pp(string.gsub(args.eval, "<", "&lt;"))
+  end
+  pp("</textarea>")
+  pp("</form>")
   
   su.dstrcat(dd, "</body></html>")
 
